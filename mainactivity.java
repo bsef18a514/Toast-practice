36d65656e83f1e47bec5e17f03bb94168df8dc15
bsef18a514 <91655339+bsef18a514@users.noreply.github.com>
2021-12-08 12:00:02 +0500

    display change

--- mainactivity.java
+++ mainactivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.activity_main);  
   
-        //Displaying Toast with Hello Javatpoint message  
-        Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();  
+        
     }  
 }  
