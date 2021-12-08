bbc195626d656a3e1e733d7976816141fc4400e0
bsef18a514 <91655339+bsef18a514@users.noreply.github.com>
2021-12-08 12:00:50 +0500

    print hello message

--- mainactivity.java
+++ mainactivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {  
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.activity_main);  
+      //
+       //Displaying Toast with Hello message  
+        Toast.makeText(getApplicationContext(),"Hello Message",Toast.LENGTH_SHORT).show(); 
   
         
     }  
